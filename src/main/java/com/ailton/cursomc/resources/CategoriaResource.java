1bf3d2753de6aea8135c37afee716799dd326a11
Ailton Moreira <ailtonfiasco2@gmail.com>
2019-08-20 20:42:55 -0100

    Criando repository e service para Categoria

--- src/main/java/com/ailton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ailton/cursomc/resources/CategoriaResource.java
@@ -3,34 +3,39 @@ package com.ailton.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ailton.cursomc.domain.Categoria;
+import com.ailton.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 
 	//	public String listar()
 	
-	public List<Categoria> listar() {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática"); 
-		Categoria cat2 = new Categoria (2, "Escritório");
-	
+		Categoria obj = service.buscar(id);
 		
-	
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-				
+		return ResponseEntity.ok().body(obj);
+		//Categoria cat1 = new Categoria(1, "Informática"); 
+		//Categoria cat2 = new Categoria (2, "Escritório");
+		//List<Categoria> lista = new ArrayList<>();		
+		//lista.add(cat1);
+		//lista.add(cat2);
+		//return lista;
 		//return "Rest está funcionando!";
 	}
 	
