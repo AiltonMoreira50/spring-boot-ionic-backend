95e85e9686f11d42dd7020334d22df7e91ae88d2
Ailton Moreira <ailtonfiasco2@gmail.com>
2019-09-17 15:41:30 -0100

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/ailton/cursomc/domain/Endereco.java
+++ src/main/java/com/ailton/cursomc/domain/Endereco.java
@@ -12,7 +12,6 @@ import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-
 public class Endereco implements Serializable {
     
 	private static final long serialVersionUID = 1L;
