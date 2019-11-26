abb320cf6053b70ef637b01c03bb3c30a08dd4ca
Ailton Moreira <ailtonfiasco2@gmail.com>
2019-11-26 16:23:36 -0100

    Inserido uma ova Categoria com POST

--- src/main/java/com/ailton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ailton/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.ailton.cursomc.resources;
 
+import java.net.URI;
+
 //import java.util.ArrayList;
 //import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ailton.cursomc.domain.Categoria;
 import com.ailton.cursomc.services.CategoriaService;
@@ -40,5 +44,12 @@ public class CategoriaResource {
 	}
 	
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 }
